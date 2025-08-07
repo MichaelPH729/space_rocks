44ec2e6e6d76001b9ba010ef9ff2388d6563178d
MichaelPH729 <michaelpaulhodges@gmail.com>
2025-08-07 04:31:45 -0400

    cleaning up the code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pygame
 from constants import *
-clock = pygame.time.Clock()
-dt = 0
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +14,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 if __name__ == "__main__":
